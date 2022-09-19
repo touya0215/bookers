2b52d07f3d6c3d190b4d164165f98fe245f0ea26
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-09-19 10:03:08 +0000

    Start

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books/new' => 'books#new'
+  get '/books' => 'books#index'
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
+
+  get '/' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
