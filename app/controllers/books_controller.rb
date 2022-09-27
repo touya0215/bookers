ea082b91a1d9d43e265a14d3d53437674f8f3105
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-09-27 09:23:43 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,19 +14,31 @@ class BooksController < ApplicationController
     book.save
     redirect_to book_path(book.id)
   end
-  
-  def destroy 
+
+  def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
-  end 
+    
+  end
 
   def show
-    @book = Book.all
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end 
+  
+  
+  
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
 
 
-
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
